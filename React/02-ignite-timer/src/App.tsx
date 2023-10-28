f6cfcacb9643d3930aedae2cad30da8bc23dc4ff
Arthur Teixeira Serafim <arthurteixeiraserafim@gmail.com>
2023-10-28 00:46:58 -0300

    configurando eslint, estilos globais, criando rotas e layout padrão

--- React/02-ignite-timer/src/App.tsx
+++ React/02-ignite-timer/src/App.tsx
@@ -1,18 +1,15 @@
-import { Button } from "./components/Button";
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { Router } from './components/Router'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variants="primary"/>
-      <Button variants="secondary"/>
-      <Button variants="danger"/>
-      <Button variants="success"/>
-      <Button />
-
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyle />
     </ThemeProvider>
